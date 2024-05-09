4cd1da777a97edd2e4401120dac868acaa379d8b
Mark Keller <8452750+keller00@users.noreply.github.com>
2024-05-09 14:37:23 -0700

    fix new lsp-zero's removed function

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -11,9 +11,10 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-Space>'] = cmp.mapping.complete(),
 })
 
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
+-- REMOVED for info see: https://lsp-zero.netlify.app/v2.x/reference/deprecated.html#setup-nvim-cmp-opts
+-- lsp.setup_nvim_cmp({
+--     mapping = cmp_mappings
+-- })
 
 lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
