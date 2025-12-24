9b8b689e6b2b7360c0ceab56e62d68056c256566
Mark Keller <8452750+keller00@users.noreply.github.com>
2025-12-24 16:16:35 -0500

    add opencode instead of cursor-agent

--- .config/nvim/lua/plugins/cursor-agent.nvim.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "keller00/cursor-agent.nvim",
-  config = function()
-    vim.keymap.set("n", "<leader>a", ":CursorAgent<CR>", { desc = "Cursor Agent: Toggle terminal" })
-    vim.keymap.set("v", "<leader>a", ":CursorAgentSelection<CR>", { desc = "Cursor Agent: Send selection" })
-    vim.keymap.set("n", "<leader>A", ":CursorAgentBuffer<CR>", { desc = "Cursor Agent: Send buffer" })
-  end,
-}
