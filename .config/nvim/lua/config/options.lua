041acace6f8c27c4ac940babb302268dac4cd452
Mark Keller <8452750+keller00@users.noreply.github.com>
2026-01-12 20:42:38 -0800

    put OSC-52 setting behind second env variable

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,5 @@
 vim.g.autoformat = false
-if vim.env.TERM == "xterm-ghostty" then
+if vim.env.TERM == "xterm-ghostty" and vim.env.CWS == "true" then
   vim.opt.clipboard = 'unnamedplus'
   vim.g.clipboard = 'osc52'
 end
